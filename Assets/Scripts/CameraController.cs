0f19fd63f8ca9d2945b8c04705c1dc9999eae9f5
Alexandr <alex903_90@mail.ru>
2021-10-22 01:17:08 +0700

    Перекрасил дерево. Пофиксил скрипт монстров. Добавлены платформы
    Теперь монстры из многих текстур тоже могут поворачиваться. Также исправлен баг, когда они не возвращались на прежнюю высоту патрулирования после агра. Добавлены платформы на озером и соответствующий тег для них.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,20 +7,21 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
     private Vector3 pos;
-
+    private float offsetY;
 
     private void Awake()
     {
         if (!player)
         {
             player = FindObjectOfType<Hero>().transform;
+            offsetY = transform.position.y;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -28,7 +29,7 @@ public class CameraController : MonoBehaviour
     {
         pos = player.position;
         pos.z = -10f;
-        pos.y = 5.62f + pos.y; // 5.62 - высота "земли".
+        pos.y = offsetY + pos.y; // offsetY - высота, на которую будет "задрана" камера.
         transform.position = Vector3.Lerp(transform.position, pos, 2f * Time.deltaTime);
         //transform.position = Vector3.Lerp(transform.position, pos, 1f/256f);
     }
